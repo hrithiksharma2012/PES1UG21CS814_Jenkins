6c19764e0fc60d8043c0666116ec6c5adcdcd13e
Hrithik Sharma <104514515+hrithiksharma2012@users.noreply.github.com>
2023-02-17 19:58:39 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I PES1UG21CS814 have commited the changes\n";
+  cout << "Successsfully created jenkins pipeline";
   return 0;
 }
